cf3ef19d087bde241797c66467391abb8459c6b8
rakib53 <rakibdev53@gmail.com>
2025-02-23 20:50:14 +0600

    added env file

--- mini-media-backend/src/app/config/index.ts
+++ mini-media-backend/src/app/config/index.ts
@@ -1,6 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
 const config = {
-  node_env: "development",
-  port: 5000,
+  node_env: process.env.NODE_ENV, // development
+  port: process.env.PORT,
   local_url: "mongodb://127.0.0.1:27017/learning-socket",
   remote_url:
     "mongodb+srv://learning_socket:jFacE3xmEzn3Ptwi@simple-node-app.ybb3hyi.mongodb.net/learning-socket",
